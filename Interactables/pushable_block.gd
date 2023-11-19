a598568030d3d632e850350d8322d3ab3ec4c67b
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2023-11-19 13:49:32 -0500

    Progress

--- Interactables/pushable_block.gd
+++ Interactables/pushable_block.gd
@@ -1,5 +1,5 @@
 extends RigidBody2D
-class_name Pushable_block
+
 
 func pushable_block():
 	pass
--- scripts/world.gd
+++ scripts/world.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
